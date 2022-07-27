d93bec94d7dd74b40bbe7f8936a62db582d0479f
maxmsi <darkmeshas@gmail.com>
2022-07-27 20:02:25 +0200

    Algorithm for pair finding added

--- src/main/scala/PairFinder.scala
+++ src/main/scala/PairFinder.scala
@@ -1,12 +1,35 @@
 package io.datumo.recruitment
 
+import scala.:+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+
+
 class PairFinder {
 
-}
+  def task_algorithm(input: List[Int],target:Int): List[List[Int]] = {
+    var map: mutable.Map[Int, Int] = mutable.Map(input.groupBy(identity)
+      .view
+      .mapValues(_.size).toSeq: _*)
+    var pairs: ListBuffer[List[Int]] = ListBuffer.empty[List[Int]]
 
+    for (i <- input) {
+      if (map.contains(target - input(i)) && map(target - input(i)) != 0 && map(input(i)) != 0) {
+        pairs += List(input(i), target - input(i))
+        map(input(i)) = map(input(i)) - 1
+        map(target - input(i)) = map(target - input(i)) - 1
+      }
+    }
+    pairs.toList
+  }
+}
 
 object Tester {
   def main(args: Array[String]) = {
     println("Initial structure")
+    val input = List[Int](4, 8, 9, 0, 12, 1, 4, 2, 12, 12, 4, 4, 8, 11, 12, 0).sortWith(_ < _)
+    val pf= new PairFinder()
+    println(pf.task_algorithm(input,12))
   }
 }
--- target/scala-2.13/classes/io/datumo/recruitment/untitled.sc
+++ target/scala-2.13/classes/io/datumo/recruitment/untitled.sc
@@ -1,3 +1,28 @@
+import scala.:+
+import scala.collection.mutable
 
+val input = List[Int](4,8,9,0,12,1,4,2,12,12,4,4,8,11,12,0).sortWith(_<_)
 
-val x=5
+var target=12
+
+var map:mutable.Map[Int,Int]=mutable.Map.empty
+var pairResults:mutable.Map[Int,Int]=mutable.Map.empty
+var pairs: List[List[Int]]
+
+map=mutable.Map (input.groupBy(identity).mapValues(_.size).toSeq : _*)
+
+for(i <- input){
+  if(map.contains(target-input(i)) && map(target-input(i))!=0 && map(input(i))!=0){
+
+    pairs += (List(input(i),target-input(i)))
+
+    //  print(input(i))
+    //  println(target-input(i))
+
+    map(input(i))=map(input(i))-1
+    map(target-input(i))=map(target-input(i))-1
+    // println("map afte"+map)
+  }
+}
+
+pairs.foreach(list=>println(list))
